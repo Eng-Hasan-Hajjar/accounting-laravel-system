c94f01ac8dbb5e9c588fb8e9ecf57d8c8123365f
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2024-05-04 10:43:44 -0700

    add new files mor and more details from local commit

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CampGroundController;
-use App\Http\Controllers\ReservationController;
-
-use App\Http\Controllers\ArmanController;
+use Illuminate\Support\Facades\Auth;
 
 
 
@@ -36,20 +34,7 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 
-/////////////////////////////////////---CampGround
 
-/*
-Route::get('/campground', function () {
-    return view('backend.campGround');
-})->middleware(['auth', 'verified'])->name('campground');
-
-Route::middleware(['auth'])->group(function () {
-
-    Route::get('/campground', [CampgroundController::class, 'index'])->name('campground');
-
-});
-
-*/
 #-------------camping grounds
 
 Route::middleware(['auth'])->group(function () {
@@ -64,52 +49,74 @@ Route::middleware(['auth'])->group(function () {
 
 
 
+Route::get('/', function () {
+    return view('auth.login');
+});
 
+Auth::routes();
+//Auth::routes(['register' => false]);
 
+Route::get('/home', 'HomeController@index')->name('home');
 
-#-------------Armenia module
+Route::resource('invoices', 'InvoicesController');
 
-Route::middleware(['auth'])->group(function () {
+Route::resource('sections', 'SectionsController');
 
+Route::resource('products', 'ProductsController');
 
+Route::resource('InvoiceAttachments', 'InvoiceAttachmentsController');
 
-    Route::resource('/adminpanel/arman', ArmanController::class);
+//Route::resource('InvoicesDetails', 'InvoicesDetailsController');
 
+Route::get('/section/{id}', 'InvoicesController@getproducts');
 
+Route::get('/InvoicesDetails/{id}', 'InvoicesDetailsController@edit');
+
+Route::get('download/{invoice_number}/{file_name}', 'InvoicesDetailsController@get_file');
+
+Route::get('View_file/{invoice_number}/{file_name}', 'InvoicesDetailsController@open_file');
+
+Route::post('delete_file', 'InvoicesDetailsController@destroy')->name('delete_file');
+
+Route::get('/edit_invoice/{id}', 'InvoicesController@edit');
+
+Route::get('/Status_show/{id}', 'InvoicesController@show')->name('Status_show');
+
+Route::post('/Status_Update/{id}', 'InvoicesController@Status_Update')->name('Status_Update');
+
+Route::resource('Archive', 'InvoiceAchiveController');
+
+Route::get('Invoice_Paid','InvoicesController@Invoice_Paid');
+
+Route::get('Invoice_UnPaid','InvoicesController@Invoice_UnPaid');
+
+Route::get('Invoice_Partial','InvoicesController@Invoice_Partial');
+
+Route::get('Print_invoice/{id}','InvoicesController@Print_invoice');
+
+Route::get('export_invoices', 'InvoicesController@export');
+
+Route::group(['middleware' => ['auth']], function() {
+
+Route::resource('roles','RoleController');
+
+Route::resource('users','UserController');
 
 });
 
+Route::get('invoices_report', 'Invoices_Report@index');
+
+Route::post('Search_invoices', 'Invoices_Report@Search_invoices');
+
+Route::get('customers_report', 'Customers_Report@index')->name("customers_report");
+
+Route::post('Search_customers', 'Customers_Report@Search_customers');
+
+Route::get('MarkAsRead_all','InvoicesController@MarkAsRead_all')->name('MarkAsRead_all');
+
+Route::get('unreadNotifications_count', 'InvoicesController@unreadNotifications_count')->name('unreadNotifications_count');
+
+Route::get('unreadNotifications', 'InvoicesController@unreadNotifications')->name('unreadNotifications');
 
 
-#-------------reservation routes
-
-Route::middleware(['auth'])->group(function () {
-
-    Route::resource('/adminpanel/reservations', ReservationController::class);
-
-
-
-/*
-// عرض النموذج لإنشاء حجز جديد
-Route::get('/reservations/create', [ReservationController::class, 'create'])->name('reservations.create');
-
-// حفظ الحجز الجديد في قاعدة البيانات
-Route::post('/reservations', [ReservationController::class, 'store'])->name('reservations.store');
-Route::get('/getreservations', [ReservationController::class, 'showAll'])->name('reservations.showAll');
-
-// عرض التفاصيل لحجز معين
-Route::get('/reservations/{reservation}', [ReservationController::class, 'show'])->name('reservations.show');
-
-// عرض جميع الحجوزات
-Route::get('/reservations/all', [ReservationController::class, 'showAll'])->name('reservations.all');
-
-// عرض النموذج لتعديل حجز معين
-Route::get('/reservations/{reservation}/edit', [ReservationController::class, 'edit'])->name('reservations.edit');
-
-// تحديث بيانات الحجز في قاعدة البيانات
-Route::put('/reservations/{reservation}', [ReservationController::class, 'update'])->name('reservations.update');
-
-// حذف حجز معين من قاعدة البيانات
-Route::delete('/reservations/{reservation}', [ReservationController::class, 'destroy'])->name('reservations.destroy');
-*/
-});
+Route::get('/{page}', 'AdminController@index');
